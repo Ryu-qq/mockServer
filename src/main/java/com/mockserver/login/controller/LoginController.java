d521b7011db22952973e4315e7f6683d32b29237
Ryu-qq <fbtkdals2@naver.com>
2022-07-22 18:13:47 +0900

    social login mock 서버 만듬

---
+++ src/main/java/com/mockserver/login/controller/LoginController.java
@@ -0,0 +1,22 @@
+package com.mockserver.login.controller;
+
+
+import com.mockserver.login.dto.TokenResponseDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping()
+public class LoginController {
+
+
+    @GetMapping("login/redirect")
+    public ResponseEntity getAccessToken(@RequestParam("token") String accessToken){
+        return ResponseEntity.ok().body(TokenResponseDto.builder().token(accessToken).build());
+
+    }
+
+}
---
+++ src/main/java/com/mockserver/login/dto/TokenResponseDto.java
@@ -0,0 +1,15 @@
+package com.mockserver.login.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TokenResponseDto {
+    private String token;
+}
