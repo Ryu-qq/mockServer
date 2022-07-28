c5316654801f96aea1e6a17fe661670853685ef5
Ryu-qq <fbtkdals2@naver.com>
2022-07-28 18:45:22 +0900

    cookie 널

--- src/main/java/com/mockserver/login/WebMvcConfig.java
+++ src/main/java/com/mockserver/login/WebMvcConfig.java
@@ -2,7 +2,9 @@ package com.mockserver.login;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -11,6 +13,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
                 .allowedOrigins("https://3c4f-211-192-187-70.jp.ngrok.io")
+                .allowedOrigins("http:localhost:8089")
                 .allowedOrigins("https://app-api-stg-beborn-prod.apps.beborn-cluster.4p30.p1.openshiftapps.com")
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST")
