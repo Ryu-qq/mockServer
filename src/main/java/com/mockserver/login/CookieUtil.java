61df00b07b023049e82bd63190f5b370cc449765
Ryu-qq <fbtkdals2@naver.com>
2022-07-28 18:22:45 +0900

    cookie 널

--- src/main/java/com/mockserver/login/CookieUtil.java
+++ src/main/java/com/mockserver/login/CookieUtil.java
@@ -11,8 +11,8 @@ import java.util.Optional;
 public class CookieUtil {
 
     public static Optional<Cookie> getCookie(HttpServletRequest request, String name) {
+
         Cookie[] cookies = request.getCookies();
-        System.out.println("cookies = " + cookies.length);
         if (cookies != null && cookies.length > 0) {
             for (Cookie cookie : cookies) {
                 if (name.equals(cookie.getName())) {
---
+++ src/main/java/com/mockserver/login/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.mockserver.login;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("https://3c4f-211-192-187-70.jp.ngrok.io")
+                .allowedMethods("GET", "POST")
+                .maxAge(3600);
+    }
+}
