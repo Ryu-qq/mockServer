360e25c26cbcf49eb935b69b851ba0d59d0311db
Ryu-qq <fbtkdals2@gmail.com>
2022-07-31 22:22:05 +0900

    cookie 이슈

--- src/main/java/com/mockserver/login/controller/ProviderController.java
+++ src/main/java/com/mockserver/login/controller/ProviderController.java
@@ -32,7 +32,8 @@ public class ProviderController {
     public String redirectLoginPage3(HttpServletRequest request, HttpServletResponse response,
                                      @ModelAttribute AuthorizationTokenRequestDto authorizationTokenRequestDto,
                                      RedirectAttributes redirectAttributes){
-        System.out.println("oauth/authorize = " + request.getHeader("x-forwarded-for"));
+
+        System.out.println("request = " + getClientIP(request));
 
         String[] codeList = {"code"};
         String[] stateList = {authorizationTokenRequestDto.getState()};
@@ -68,6 +69,35 @@ public class ProviderController {
 
         return ResponseEntity.ok().body(map);
 
+    }
+
+    public String getClientIP(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+        log.info("> X-FORWARDED-FOR : " + ip);
+
+        if (ip == null) {
+            ip = request.getHeader("Proxy-Client-IP");
+            log.info("> Proxy-Client-IP : " + ip);
+        }
+        if (ip == null) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+            log.info(">  WL-Proxy-Client-IP : " + ip);
+        }
+        if (ip == null) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+            log.info("> HTTP_CLIENT_IP : " + ip);
+        }
+        if (ip == null) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+            log.info("> HTTP_X_FORWARDED_FOR : " + ip);
+        }
+        if (ip == null) {
+            ip = request.getRemoteAddr();
+            log.info("> getRemoteAddr : "+ip);
+        }
+        log.info("> Result : IP Address : "+ip);
+
+        return ip;
     }
 
 }
